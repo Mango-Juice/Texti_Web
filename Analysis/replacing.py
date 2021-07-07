d48dec3e0e3cf38b7acc790c2500b0fc99d817d1
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-07-07 20:19:09 +0900

    이중피동 제거 함수

---
+++ Analysis/replacing.py
@@ -0,0 +1,53 @@
+from konlpy.tag import Kkma
+kma = Kkma()
+
+def RemoveDoublePassive(input_text):
+    # 이중피동 제거 함수
+    replacing = ["이", "히", "리", "기"]
+    words = input_text.split(" ")
+    result = ""
+
+    for word_idx, word in enumerate(words):
+        changed_word = ""
+        is_changed = False
+        passCount = 0
+        tagged_morphs = kma.pos(word)
+
+        for morph_idx, morph in enumerate(tagged_morphs):
+            flag = True
+
+            if passCount > 0:
+                passCount -= 1
+                continue
+
+            if flag and morph[1] == "VV":
+                if morph_idx < len(tagged_morphs) - 2 and tagged_morphs[morph_idx + 1][1] == "ECS" and tagged_morphs[morph_idx + 2][1] == "VXV":
+                    for rep_idx, replaced in enumerate(["이어지", "히어지", "리어지", "기어지"]):
+                        target = morph[0] + tagged_morphs[morph_idx+1][0] + tagged_morphs[morph_idx+2][0]
+                        for idx in range(len(target)-2):
+                            if replaced == target[idx:idx+3]:
+                                changed_word += target[:idx] + replacing[rep_idx]
+                                passCount = 2
+                                is_changed = True
+                                flag = False
+                else:
+                    for rep_idx, replaced in enumerate(["여지", "혀지", "려지", "겨지"]):
+                        target = morph[0]
+                        for idx in range(len(target)-1):
+                            if replaced == target[idx:idx+2]:
+                                changed_word += target[:idx] + replacing[rep_idx]
+                                is_changed = True
+                                flag = False
+
+                if flag:
+                    changed_word += morph[0]
+
+            else:
+                changed_word += morph[0]
+
+        if is_changed:
+            result += " " + changed_word
+        else:
+            result += " " + word
+
+    return result
