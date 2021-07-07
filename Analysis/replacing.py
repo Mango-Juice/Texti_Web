d4a558a2424ceff0fba1cc117d2b6d51879ad3df
CaChiJ <53603478+CaChiJ@users.noreply.github.com>
2021-07-08 01:52:02 +0900

    이중복수 제거함수

--- Analysis/replacing.py
+++ Analysis/replacing.py
@@ -2,6 +2,74 @@ from konlpy.tag import Kkma
 kma = Kkma()
 
 
+def remove_double_plural(input_text):
+    # 이중 복수 제거 함수
+
+    plural_table = ((('수많', 'VA'), ('은', 'ETD'), ('<_AL_>', 'NNG'), ('들', 'XSN')),
+                    (('수많', 'VA'), ('은', 'ETD'), ('<_AL_>', 'NNP'), ('들', 'XSN')),
+                    (('수많', 'VA'), ('은', 'ETD'), ('<_AL_>', 'UN'), ('들', 'XSN')),
+                    (('많', 'VA'), ('은', 'ETD'), ('<_AL_>', 'NNG'), ('들', 'XSN')),
+                    (('많', 'VA'), ('은', 'ETD'), ('<_AL_>', 'NNP'), ('들', 'XSN')),
+                    (('많', 'VA'), ('은', 'ETD'), ('<_AL_>', 'UN'), ('들', 'XSN')),
+                    (('여러', 'MDT'), ('<_AL_>', 'NNG'), ('들', 'XSN')),
+                    (('여러', 'MDT'), ('<_AL_>', 'NNP'), ('들', 'XSN')),
+                    (('여러', 'MDT'), ('<_AL_>', 'UN'), ('들', 'XSN')),
+                    (('무리', 'NNG'), ('들', 'XSN'))
+                    )
+
+    result = ""
+    pass_count = 0
+
+    for line in input_text.split(". "):
+        words = line.split(" ")
+
+        for word_idx, word in enumerate(words):
+
+            if pass_count > 0:
+                pass_count -= 1
+                continue
+
+            morphs = (kma.pos(word))
+            is_pattern_same = True
+            used_word = 1
+
+            for case in plural_table:
+                for c, (rep, tag) in enumerate(case):
+                    if len(morphs) <= c:
+                        break
+
+                    morph_check = rep != '<_AL_>' and rep != morphs[c][0]
+
+                    if tag != morphs[c][1] and morph_check:
+                        is_pattern_same = False
+                        break
+
+                    if len(morphs) <= c + 1 and len(words) > word_idx+1:
+                        morphs.extend(kma.pos(words[word_idx+1]))
+                        used_word += 1
+
+                if is_pattern_same:
+                    break
+
+            if is_pattern_same:
+                pass_count = used_word - 1
+
+                for i in range(used_word-1):
+                    result += words[word_idx + i]
+                    result += ' '
+
+                result += words[word_idx + used_word - 1][:words[word_idx + used_word - 1].rfind('들')] + words[word_idx + used_word - 1][words[word_idx + used_word - 1].rfind('들') + 1:]
+                result += ' '
+            else:
+                result += word
+                result += ' '
+
+    return result
+
+
+print(remove_double_plural("나는 여러 선풍기들과 함께 갔다."))
+
+
 def remove_double_passive(input_text):
     # 이중피동 제거 함수
     replacing = ["이", "히", "리", "기"]
